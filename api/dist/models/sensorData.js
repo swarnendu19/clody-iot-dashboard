3db9027bec6ff866f80313efdac3944728953ed1
Swarnendu <swarnendumaity633@gmail.com>
2025-04-02 10:39:08 +0530

    fixed .gitignore

--- api/dist/models/sensorData.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.addSensorData = void 0;
-const db_1 = require("../db");
-const addSensorData = async (device_id, temperature, humidity) => {
-    const res = await db_1.pool.query('INSERT INTO sensor_data (device_id, temperature, humidity, timestamp) VALUES ($1, $2, $3, NOW()) RETURNING *', [device_id, temperature, humidity]);
-    return res.rows[0];
-};
-exports.addSensorData = addSensorData;
--- api/dist/server.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const subscriber_1 = require("./subscriber");
-// Main server logic
-console.log('Server: Starting...');
-// Register callback to print data from subscriber
-subscriber_1.subscriber.onData((data) => {
-    console.log(`Server: Received - Temperature: ${data.temperature.toFixed(1)}°C, Humidity: ${data.humidity.toFixed(1)}%`);
-});
